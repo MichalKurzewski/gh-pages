c3d097619221484f190f0caa863472b872086c54
Michal Kurzewski <kurzewski.m@gmail.com>
2023-03-09 12:01:25 +0000

    gh-deploy-setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/name-of-my-repository/",
+  base: "/gh-pages/",
   plugins: [react()],
 });
